6c1ef13d66e86f7e48ce6daf82267ae1ecb4820b
daphycisist <daphycisist@gmail.com>
2023-08-02 09:40:44 +0100

    feat: Add Dashoard and Sidebar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,16 +1,15 @@
+import { UserButton } from '@clerk/nextjs';
 import { Menu } from 'lucide-react';
 import { Button } from '../ui/button';
-import { UserButton } from '@clerk/nextjs';
+import MobileSidebar from '../MobileSidebar';
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-          </Button>
-          <div className='flex justify-end w-full'>
-              <UserButton afterSignOutUrl='/' />
-          </div>
+     <MobileSidebar />
+      <div className="flex justify-end w-full">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
   );
 };
--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,13 +1,61 @@
-'use-client';
+'use client';
 
 import { cn } from '@/lib/utils';
+import {
+  Code,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  Settings,
+  VideoIcon,
+} from 'lucide-react';
 import { Montserrat } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
-
+import { usePathname } from 'next/navigation';
 const montserrat = Montserrat({ weight: '600', subsets: ['latin'] });
 
+const routes = [
+  {
+    label: 'Dashboard',
+    icon: LayoutDashboard,
+    href: '/dashboard',
+    color: 'text-sky-500',
+  },
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    href: '/conversation',
+    color: 'text-violet-500',
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    href: '/image',
+    color: 'text-pink-700',
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    href: '/video',
+    color: 'text-emerald-500',
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    href: '/code',
+    color: 'text-green-700',
+  },
+  {
+    label: 'Settings',
+    icon: Settings,
+    href: '/settings',
+    color: 'text-gray-400',
+  },
+];
+
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <div className="flex flex-col h-full py-4 space-y-4 bg-[#111827] text-white">
       <div className="flex-1 px-3 py-2">
@@ -15,8 +63,29 @@ const Sidebar = () => {
           <div className="relative w-8 h-8 mr-4">
             <Image fill alt="Logo" src="/logo.png" />
           </div>
-          <h1 className={cn("text-2xl font-bold", montserrat.className)}>CreaThinkAI</h1>
+          <h1 className={cn('text-2xl font-bold', montserrat.className)}>
+            CreaThinkAI
+          </h1>
         </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className={cn(
+                'flex justify-start w-full p-3 text-sm font-medium transition rounded-lg cursor-pointer group hover:text-white hover:bg-white/10',
+                pathname === route?.href
+                  ? 'text-white bg-white/10'
+                  : 'text-zinc-400'
+              )}
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
